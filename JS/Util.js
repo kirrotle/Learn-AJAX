8874acbeb19397e09e152f197d8d19f6992647a9
RDer <kirrotle@gmail.com>
2024-09-14 20:40:21 +0800

    [modify] Learn ES6 another export import

--- JS/Util.js
+++ JS/Util.js
@@ -1,13 +1,8 @@
-const indexHtml = 'https://www.google.com/'
-const sum =
+export const indexHtml = 'https://www.google.com/'
+export const sum =
   function (arr) {
     let result = arr.reduce(function (current, val) {
       return current += val
     }, 0)
     return result
   }
-
-export default {
-  index: indexHtml,
-  sumFun: sum
-}
--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,9 @@
-import util from './Util.js'
+import { indexHtml as index, sum as sumFun } from './Util.js'
+import * as util from './Util.js'
+
+console.log(index)
+console.log(sumFun([1, 2, 3, 4, 5]))
 
 console.log(util)
-console.log(util.index)
-console.log(util.sumFun([1, 2, 3, 4, 5]))
+console.log(util.indexHtml)
+console.log(util.sum([1, 2, 3, 4, 5]))

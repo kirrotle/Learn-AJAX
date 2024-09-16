d67e62392a88360c294f67c4ce530d5ecebffd73
RDer <kirrotle@gmail.com>
2024-09-16 19:17:03 +0800

    [New] Learn Webpack bundle package

---
+++ JS/package/src/check/checkPhone.js
@@ -0,0 +1,7 @@
+export const checkPhone = (phone) => {
+  return phone <= 8
+}
+
+export const checkNumber = (num) => {
+  return num <= 4
+}
---
+++ JS/package/src/index.js
@@ -0,0 +1,4 @@
+import { checkPhone, checkNumber } from "./check/checkPhone";
+
+console.log(checkPhone(123))
+console.log(checkNumber(123999999999999))
